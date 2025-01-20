f0f8ef56f0596fc4f4ab41e0eb20b039cefba816
Christopher O'Connor <christopher.oconnor1@hotmail.com>
2025-01-20 19:07:34 +0000

    basic navbar and css along with shell of hero section

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,8 @@
+export default function Hero() {
+    return (
+        <div>
+            <h1>Hi I am a Developer </h1>
+            <p>backstory</p>
+        </div>
+    )
+}
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import "./Navbar.css"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <ul>
+                <li><a href="Home">Home</a></li>
+                <li><a href="About">About</a></li>
+                <li><a href="Projects">Projects</a></li>
+                <li><a href="Contact">Contact Me!</a></li>
+            </ul>
+        </nav>
+    )
+}
